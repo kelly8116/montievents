a4385f7f1151b9049e1c3938ce5509c791fa5cb2
kelly8116 <kelly8116@yahoo.com>
2020-01-28 12:30:17 -0600

    more

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  FB_ACCESS_TOKEN: '"2209799002607600|fc5789f70e6e006141b05babb171a25b"'
+  FB_ACCESS_TOKEN: '"EAAfZAzOyTTZCABANZBO0GQknaphyCjSjpnfDnkB549qULyoVRp2cNrQjEHorbSEGHmgwcMTFFDkMV9S6Ne5d6MZAKIGcPB6ZCFDJpEDrKlOclCw34Jy7FsVbFSKKf4C7aehblxZCvzAQC0ZAM2nJ5jPcNicYzgxu6B7DsdJaknSLV225DUL4ys4y5WI1ZBqYWhTdb2Pjgbdd7AZDZD"'
 })
