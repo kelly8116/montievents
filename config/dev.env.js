0d85018e9ebced197ca5fa0b8306e0cb9a8805a2
kelly8116 <kelly8116@yahoo.com>
2020-01-29 06:13:13 -0600

    again

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  FB_ACCESS_TOKEN: '"2209799002607600|fc5789f70e6e006141b05babb171a25b"'
+  FB_ACCESS_TOKEN: '"2209799002607600|00ba2e9ebe8362bc257e6f67d0b53cee"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  FB_ACCESS_TOKEN: '"2209799002607600|fc5789f70e6e006141b05babb171a25b"'
+  FB_ACCESS_TOKEN: '"2209799002607600|00ba2e9ebe8362bc257e6f67d0b53cee"'
 }
